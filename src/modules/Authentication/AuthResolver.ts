dc8091855a7d595a7ec6842bd254eca6f482d8da
Everton Pereira <everton.mwp@gmail.com>
2019-09-22 22:36:20 -0300

    make resolvers leaner

--- src/modules/Authentication/AuthResolver.ts
+++ src/modules/Authentication/AuthResolver.ts
@@ -1,19 +1,14 @@
-import { Resolver, Query, Mutation, Arg, Ctx, UseMiddleware } from 'type-graphql'
-import { hash, compare } from 'bcryptjs'
-import { MyContext } from '../../MyContext'
-import { User } from '../../entities/User'
+import { Resolver, Query, Mutation, Arg, UseMiddleware } from 'type-graphql'
 import { UserResponse, LoginResponse } from '../../entities/types'
 import { isAuth } from './isAuth'
-import { createAccessToken } from './authToken'
+import { AuthService } from './AuthService'
 
 @Resolver()
 export class AuthResolver {
   @Query(() => [UserResponse])
   @UseMiddleware(isAuth)
-  async users (@Ctx() { payload }: MyContext): Promise<UserResponse[]> {
-    console.log(payload)
-    const users = await User.find()
-    return users
+  async users (): Promise<UserResponse[]> {
+    return AuthService.getUsers()
   }
 
   @Mutation(() => Boolean)
@@ -21,20 +16,7 @@ export class AuthResolver {
     @Arg('email') email: string,
     @Arg('password') password: string
   ): Promise<boolean> {
-    const hashedPassword = await hash(password, 10)
-    const user = await User.findOne({ where: { email } })
-    if (user) {
-      return false
-    }
-    try {
-      await User.insert({
-        email,
-        password: hashedPassword
-      })
-      return true
-    } catch (err) {
-      return false
-    }
+    return AuthService.register(email, password)
   }
 
   @Mutation(() => LoginResponse)
@@ -42,20 +24,6 @@ export class AuthResolver {
     @Arg('email') email: string,
     @Arg('password') password: string
   ): Promise<LoginResponse> {
-    const user = await User.findOne({ where: { email } })
-
-    if (!user) {
-      throw new Error('Invalid credentials')
-    }
-
-    const valid = await compare(password, user.password)
-
-    if (!valid) {
-      throw new Error('Invalid credentials')
-    }
-
-    return {
-      accessToken: createAccessToken(user)
-    }
+    return AuthService.login(email, password)
   }
 }
