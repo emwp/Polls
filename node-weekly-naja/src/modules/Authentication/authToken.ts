478b55e6374dfa61ae74b9f04282dc96e95d458c
Everton Pereira <everton.mwp@gmail.com>
2019-09-25 23:36:00 -0300

    Extend accessToken duration

--- node-weekly-naja/src/modules/Authentication/authToken.ts
+++ node-weekly-naja/src/modules/Authentication/authToken.ts
@@ -3,5 +3,5 @@ import { sign } from 'jsonwebtoken'
 
 export const createAccessToken = (user: User) => {
   const accessSecret = process.env.ACCESS_TOKEN_SECRET || 'supersecretsecret'
-  return sign({ userId: user.id }, accessSecret, { expiresIn: '15m' })
+  return sign({ userId: user.id }, accessSecret, { expiresIn: '30d' })
 }
