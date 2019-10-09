23a459ea4e325ba284a0e4d3a0b366e9b9105a58
Everton Pereira <everton.mwp@gmail.com>
2019-10-09 00:17:12 -0300

    Add and configure React Navigation

--- PollsApp/App.tsx
+++ PollsApp/App.tsx
@@ -1,19 +1,40 @@
-import React from 'react'
-import { SafeAreaView, ScrollView, View, Text, StatusBar } from 'react-native'
+import React, { FC } from 'react'
+import { View, Text, Button } from 'react-native'
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator, NavigationStackProp, NavigationStackScreenComponent } from 'react-navigation-stack'
 
-const App = () => {
+interface Props {
+  navigation: NavigationStackProp
+}
+
+const DetailsScreen: FC<Props> = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView contentInsetAdjustmentBehavior="automatic">
-          <View>
-            <Text>PollsApp</Text>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
   )
 }
 
-export { App }
+const HomeScreen: FC<Props> = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ marginBottom: 20 }}>Home Screen</Text>
+      <Button
+        title='Go To Details Screen'
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  )
+}
+
+const AppNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen
+  },
+  {
+    initialRouteName: 'Home'
+  }
+)
+
+export default createAppContainer(AppNavigator)
--- node-polls-app/src/modules/Poll/service/GetUserPolls/GetUserPolls.ts
+++ node-polls-app/src/modules/Poll/service/GetUserPolls/GetUserPolls.ts
@@ -1,10 +1,6 @@
 import { Poll } from '../../../../entities/Poll'
 
 export const GetUserPolls = async (userId: string) : Promise<Poll[]> => {
-  try {
-    const polls = await Poll.find({ where: { user: userId }, order: { name: 'ASC' } })
-    return polls
-  } catch (_) {
-    throw new Error('Unable to retrieve user polls')
-  }
+  const polls = await Poll.find({ where: { user: userId }, order: { name: 'ASC' } })
+  return polls
 }
